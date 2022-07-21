fb59b8edfb5b97ebfa4606c8176f181193d01b64
mfebrindafian <mfebrindafian16@gmail.com>
2022-07-21 14:25:31 +0700

    tambah dan list berita

--- app/Models/MasterBeritaModel.php
+++ app/Models/MasterBeritaModel.php
@@ -9,5 +9,15 @@ class MasterBeritaModel extends Model
     protected $useTimestamps = true;
     protected $useSoftDeletes = true;
     protected $table = 'mst_berita';
-    protected $allowedFields = ['user_id', 'judul_berita', 'penulis', 'satker_kd', 'tgl_upload', 'status', 'tgl_publish', 'link_publish', 'editor', 'tgl_mulai_review', 'tgl_selesai_review', 'file_draft', 'file_review', 'image_upload'];
+    protected $allowedFields = ['user_id', 'judul_berita', 'penulis', 'satker_kd', 'tgl_upload', 'status_kd', 'tgl_publish', 'link_publish', 'editor', 'tgl_mulai_review', 'tgl_selesai_review', 'file_draft', 'file_review', 'image_upload'];
+
+    public function getAllBeritaByPenulis($user_id)
+    {
+        return $this
+            ->table('mst_berita')
+            ->select('*')
+            ->where('user_id', $user_id)
+            ->get()
+            ->getResultArray();
+    }
 }
