6743da380f209373c61c6ad98587d78db669f4a6
M Febrin Dafian <83019762+mfebrindafian@users.noreply.github.com>
2023-12-13 09:08:45 +0700

    Update MasterUserSeeder.php

--- app/Database/Seeds/MasterUserSeeder.php
+++ app/Database/Seeds/MasterUserSeeder.php
@@ -10,16 +10,16 @@ class MasterUserSeeder extends Seeder
     public function run()
     {
         $faker = \Faker\Factory::create();
-        $pass = password_hash('coolhand', PASSWORD_DEFAULT);
+        $pass = password_hash('', PASSWORD_DEFAULT);
         $data = [
-            'username' => 'adminbps',
-            'fullname' => 'bps1500',
-            'email' => 'bps1500@gmail.com',
+            'username' => '',
+            'fullname' => '',
+            'email' => '',
             'password' => $pass,
-            'token' => 'coolhand',
-            'image' => 'image.jpg',
-            'nip_lama_user' => '123456789',
-            'is_active' => 'Y',
+            'token' => '',
+            'image' => '',
+            'nip_lama_user' => '',
+            'is_active' => '',
         ];
 
         $this->db->table('tbl_user')->insert($data);
